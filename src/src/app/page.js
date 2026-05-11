04c1a95004839fa06e255944d815658c164c0bd3
JAJA-fiel456 <joaojacomo341@gmail.com>
2026-05-11 00:41:26 -0300

    Add files via upload

---
+++ src/src/app/page.js
@@ -0,0 +1,12 @@
+import { TopbarMenu } from "@/components/ui/topbar-menu"
+import { Card } from "@/components/ui/card"
+
+export default function Home() {
+  return (
+    <>
+      <TopbarMenu />
+      <Card />
+      <h1>Home</h1>
+    </>
+  )
+}
