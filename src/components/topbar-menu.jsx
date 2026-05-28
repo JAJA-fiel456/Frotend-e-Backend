1c2e0757d9e37d564893a768a8667e7def2e1538
JAJA-fiel456 <joaojacomo341@gmail.com>
2026-05-28 14:36:34 -0400

    Add files via upload

--- src/components/topbar-menu.jsx
+++ src/components/topbar-menu.jsx
@@ -16,7 +16,7 @@ export default function TopbarMenu() {
   return (
     <header className="sticky top-0 z-50 flex items-center gap-9 h-20">
       <h1 className="text-5xl font-extrabold">
-        <Link href="/">minURL</Link>
+        <Link href="/">MEDKIT</Link>
       </h1>
       <nav className="flex justify-between items-center w-full">
         <div className="hidden sm:flex gap-6 text-sm">
@@ -37,17 +37,7 @@ export default function TopbarMenu() {
           >
             Cadastre-se
           </Link>
-          <Button
-            variant="outline"
-            size="icon"
-            className="relative"
-            onClick={handleToggleTheme}
-            aria-label="Alternar tema"
-          >
-            <Sun className="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all duration-300 ease-out dark:scale-0 dark:-rotate-90" />
-            <Moon className="absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 transition-all duration-300 ease-out dark:scale-100 dark:rotate-0" />
-            <span className="sr-only">Toggle theme</span>
-          </Button>
+          
         </div>
       </nav>
     </header>
