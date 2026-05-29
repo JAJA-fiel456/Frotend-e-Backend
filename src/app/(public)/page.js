4e004f54644885ece4b4feec8822b1c9a5d13e55
JAJA-fiel456 <joaojacomo341@gmail.com>
2026-05-28 23:41:23 -0400

    Add files via upload

--- src/app/(public)/page.js
+++ src/app/(public)/page.js
@@ -1,9 +1,16 @@
-import { TabsFormUrl } from "@/components/ui/tabs-form-url";
+import Image from "next/image";
 
 export default function Home() {
   return (
     <section className="flex flex-col justify-center items-center h-[calc(100vh-5rem)] max-h-screen">
-      
+      <div className="pb-8">
+        <h1 className="text-5xl font-extrabold mb-1.5 text-center">
+         Cuidando da sua saúde com confiança
+        </h1>
+         <h2 className="text-3xl font-semibold mb-10 text-center">
+          Nossa plataforma foi desenvolvida para conectar pacientes e profissionais da saúde, oferecendo acesso fácil a informações sobre medicamentos, indicações, cuidados de uso e acompanhamento médico. Aqui, você encontra conteúdos atualizados para auxiliar na promoção da saúde e do bem-estar, sempre com responsabilidade e segurança.
+        </h2>
+      </div>
       
     </section>
   );
--- src/components/topbar-menu.jsx
+++ src/components/topbar-menu.jsx
@@ -5,6 +5,7 @@ import { Button } from "./ui/button";
 import { Separator } from "./ui/separator";
 import { Moon, Sun } from "lucide-react";
 import { useTheme } from "next-themes";
+import Image from "next/image";
 
 export default function TopbarMenu() {
   const { resolvedTheme, setTheme } = useTheme();
@@ -14,10 +15,19 @@ export default function TopbarMenu() {
     setTheme(isDark ? "light" : "dark");
   };
   return (
-    <header className="sticky top-0 z-50 flex items-center gap-9 h-20">
+    <header className="sticky top-0 z-50 flex items-center gap-20 h-20">
       <h1 className="text-5xl font-extrabold">
-        <Link href="/">MEDKIT</Link>
-      </h1>
+  <Link href="/" className="flex items-center gap-2">
+    <Image
+      src="/download.png"
+      alt="Logo MedKit"
+      width={50}
+      height={50}
+      priority
+    />
+    <span>MEDKIT</span>
+  </Link>
+</h1>
       <nav className="flex justify-between items-center w-full">
         <div className="hidden sm:flex gap-6 text-sm">
           <Link href="/plans">Planos</Link>
